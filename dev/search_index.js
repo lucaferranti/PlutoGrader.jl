1d9f1633db790287f0fe2f1ba7a6ee3034440483
Documenter.jl <documenter@juliadocs.github.io>
2023-10-03 15:03:17 +0000

    build based on 73ab035

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlutoGrader","category":"page"},{"location":"#PlutoGrader","page":"Home","title":"PlutoGrader","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlutoGrader.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlutoGrader]","category":"page"}]
+}
